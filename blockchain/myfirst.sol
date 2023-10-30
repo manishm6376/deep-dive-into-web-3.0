20546cb7fad678bb4e34974ee4975eab24f7caee
Manish Meena <105962369+manishm6376@users.noreply.github.com>
2023-10-30 15:16:21 +0530

    Update myfirst.sol

--- blockchain/myfirst.sol
+++ blockchain/myfirst.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.8;
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
+
 
 contract SimpleStorage {
 
@@ -12,7 +11,7 @@ contract SimpleStorage {
         uint256 favoriteNumber;
         string name;
     }
-    // uint256[] public anArray;
+   
     People[] public people;
 
     mapping(string => uint256) public nameToFavoriteNumber;
