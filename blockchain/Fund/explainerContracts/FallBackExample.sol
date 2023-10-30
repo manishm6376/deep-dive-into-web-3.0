e89a8e88b7eb43baaa334070253d5eea1f469c31
Manish Meena <105962369+manishm6376@users.noreply.github.com>
2023-10-30 15:35:33 +0530

    Create FallBackExample.sol

---
+++ blockchain/Fund/explainerContracts/FallBackExample.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FallbackExample {
+    uint256 public result;
+
+    // Fallback function must be declared as external.
+    fallback() external payable {
+        result = 1;
+    }
+
+    receive() external payable {
+        result = 2;
+    }
+}
+
